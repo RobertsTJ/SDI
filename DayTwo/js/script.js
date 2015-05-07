a1e32113556bc3d6d5828ae3b8bd765fb52ae8c8
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-07 13:41:50 -0400

    More day 2 coding
    Added Prompt in code

--- DayTwo/js/script.js
+++ DayTwo/js/script.js
@@ -12,6 +12,7 @@ var myHomeAddress = "123 Elm Street";
 var milesToStore = 8.5;
 var construction;
 var myFavoriteStore = "WholeFoods Market";
+var stuff = prompt ("Do you like Wholefoods Market?");
 
 //adding my confirm
 // confirm();
@@ -22,4 +23,4 @@ console.log("I live at " + myHomeAddress + "."); // this is my first output
 console.log("I shop at " + myFavoriteStore + ".");
 console.log("It's only " + milesToStore + " miles to the store.");
 console.log("It's " + construction + " that there is construction enroute. ");
-
+console.log( stuff );
