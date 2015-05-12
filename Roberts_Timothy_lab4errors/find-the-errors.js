3c0d0ac321417ad7c95e36518c0a8b9c8772b2a9
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-12 15:56:06 -0400

    Lab 4: Find the Errors
    Lab 4: Find the Errors coding

--- Roberts_Timothy_lab4errors/find-the-errors.js
+++ Roberts_Timothy_lab4errors/find-the-errors.js
@@ -1,17 +1,17 @@
-//  NAME:  Enter your name
-//  DATE:  Enter the current date
-// Scalable Data Infrastructures
-//  Day 4 Lab
+//  NAME:  Timothy Roberts
+//  DATE:  May 12, 2015
+//  Scalable Data Infrastructures
+//  Day 4 Lab 00
 //  Assignment 2
 //  Find and fix the errors
 
 var myName = "John Doe";
-var my Job  = "\"Cat Wrangler\"";
+var myJob  = "\"Cat Wrangler\"";
 var myRate = 7.50;
 var numOfCats = 40;
 var employed = true;
 
-console.log("Hello!  My name is " + myName ".");
+console.log("Hello!  My name is " + myName + ".");
 console.log("I'm a " + myJob + ".");
 console.log("My current assignment has me wrangling " + numOfCats + " cats.");
 console.log("So, let's get to work!");
@@ -20,13 +20,13 @@ while (numOfCats > 0) {
     
     if (employed = true) {
         
-        console.log("I've wrangled another cat.  Only " + numofcats + " left!");
+        console.log("I've wrangled another cat.  Only " + numOfCats + " left!");
         
     } else {
         
         console.log("I've been fired!  Someone else will have to wrangle the rest!");
         
-    };
+    }
     
     numOfCats--;
     
@@ -34,7 +34,7 @@ while (numOfCats > 0) {
         
         employed = false;
         
-    };
+    }
     
-};
+}
 
