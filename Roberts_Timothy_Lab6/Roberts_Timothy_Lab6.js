0f4496083799f20a2b51042e6e55232ef3286fd2
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-16 13:44:18 -0400

    Update Lab6 added loop, return, and console.log
    Code doesn’t work properly yet but fixing it now, just a layout.

--- Roberts_Timothy_Lab6/Roberts_Timothy_Lab6.js
+++ Roberts_Timothy_Lab6/Roberts_Timothy_Lab6.js
@@ -9,10 +9,37 @@
 
 
 
+// Global Variables
+var returnPlaceAndState;
+
+
 // Function
 function myFavoriteStates() {
 
     // Local Variables
 
     var stateArray = [ "Wisconsin", "Virginia", "California", "Montana", "Florida" ];
-    var favoritePlacesArray = [ "Lambeau Field", "Virginia Beach", "Santa Monica Pier", "Glacier National Park", "Pompano Beach" ]
+    var favoritePlacesArray = [ "Lambeau Field", "Virginia Beach", "Santa Monica Pier", "Glacier National Park", "Pompano Beach" ];
+    var addState;
+    var placeAndState;
+
+
+    // For loop through the elements of the array
+    for(var i = addState ; i < stateArray.length; i++) {
+
+        addState + stateArray[i];
+
+
+    }
+
+    // Average of the total
+    placeAndState = addState / stateArray.length;
+
+    // Return the final average
+    return (placeAndState);
+
+}
+
+returnPlaceAndState = myFavoriteStates();
+
+console.log("My favorite places in these states are " + returnPlaceAndState + ".");
