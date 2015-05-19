117442c5a81b233fd198e0f282863bf831b1e25d
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-19 15:21:18 -0400

    commit 2 for lab 7
    Added second for loop

--- Roberts_Timothy_Lab7/Roberts_Timothy_Lab7.js
+++ Roberts_Timothy_Lab7/Roberts_Timothy_Lab7.js
@@ -11,6 +11,8 @@
 
 // Global Variables
 var set1 = [10, 6, 7, 3, 12, 5];
+var set2 = [25, 100, 4, 22, 16, 105, 2, 7];
+
 var returnedArray = [];
 
 
@@ -20,16 +22,19 @@ function bubbleSort(numArgArray) {
     // Local Variables
     var holdNum;
 
-    for (var i = 0; i < set1.length; i++) {
-        for (var t = 0; t < set1.length; t++)
+    for (var i = 0; i < numArgArray.length; i++) {
+        for (var t = 0; t < numArgArray.length; t++)
 
-        if (set1[t] > set1[t+1]) {
+        if (numArgArray[t] > numArgArray[t+1]) {
             holdNum = numArgArray[t];
             numArgArray[t] = numArgArray[t+1];
             numArgArray[t+1] = holdNum;
 
         }
     }
+
+
+
 
     // console.log(numArgArray);
     return numArgArray;
@@ -39,3 +44,12 @@ function bubbleSort(numArgArray) {
 returnedArray = bubbleSort(set1);
 console.log("The new order of the array is: " + returnedArray);
 
+for (var i = 0; i < 3; i++) {
+    if (w = 1) {
+        returnedArray = bubbleSort(set1);
+        console.log("The new order of the array is: " + returnedArray);
+    }
+
+
+
+}
