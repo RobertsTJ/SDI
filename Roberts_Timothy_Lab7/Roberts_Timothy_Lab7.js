4f78a752ffc1cd533f52400d46b4548a1ff6005d
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-19 13:50:34 -0400

    Commit 1 for Lab 7
    Code set up and running right

---
+++ Roberts_Timothy_Lab7/Roberts_Timothy_Lab7.js
@@ -0,0 +1,34 @@
+/* Timothy Roberts
+   SDI 1505
+   Lab 7: Functions, Loops, and Arrays
+   May 19, 2015
+ */
+
+// alert("JavaScript works!");
+
+
+
+
+// Global Variables
+var set1 = [10, 6, 7, 3, 12, 5];
+var returnedArray = [];
+
+
+// Functions
+function bubbleSort(numArg) {
+
+    // Local Variables
+    var newArray = [];
+
+
+    for (var i = 0; i < set1.length; i++) {
+
+
+
+    }
+
+    console.log(numArg);
+}
+
+// main code
+bubbleSort(set1);
