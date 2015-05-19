342dc403c0da5e6c98d49d8cb422fe49604ada57
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-19 11:02:12 -0400

    Second commit for Array problem

--- sdi-LectureFiles-Day7/Day7Lecture.js
+++ sdi-LectureFiles-Day7/Day7Lecture.js
@@ -10,24 +10,53 @@
 var numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
 var returnedArray = [];
 
+
+
+var userPrompt = prompt("Enter 'odd' to return odd numbers or 'even' to return even numbers.");
+
+while (userPrompt != "odd" && userPrompt != "even") {
+
+       userPrompt = prompt("Hey, stupid! Enter 'odd' or 'even'");
+
+}
+
 // Function
-function evenArray(argArray) {
+function evenArray(argArray, oddOrEven) {
 
     // Local Variables
     var newArray = [];
     var modNum;
+    var zeroOrOne;
 
-    console.log(argArray);
+    if (oddOrEven === "odd") {
+
+        zeroOrOne = 1;
+    } else {
+
+        zeroOrOne = 0;
+
+    }
+
+    for (var i = 0; i < argArray.length; i++) {
+
+        modNum = argArray[i] % 2;
+
+        if (modNum === zeroOrOne) {
+
+            newArray.push(argArray[i]);
 
 
+        }
 
+    }
 
+    return newArray;
 }
 
 // Main Code
-returnedArray = evenArray(numberArray);
+returnedArray = evenArray(numberArray, userPrompt);
 
-
+console.log("The " + userPrompt + " number array is " + returnedArray + ".");
 
 
 
