afee47aa4c4ec3afa39ec4c468ee595c087887c3
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-19 10:17:58 -0400

    First commit for evenArray problem
    First commit for evenArray problem

--- sdi-LectureFiles-Day7/Day7Lecture.js
+++ sdi-LectureFiles-Day7/Day7Lecture.js
@@ -6,4 +6,89 @@
 
 // alert("JavaScript works!");
 
+// Global Variable
+var numberArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
+var returnedArray = [];
+
+// Function
+function evenArray(argArray) {
+
+    // Local Variables
+    var newArray = [];
+    var modNum;
+
+    console.log(argArray);
+
+
+
+
+}
+
+// Main Code
+returnedArray = evenArray(numberArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// Lab 6 Review
 //
+//// Function
+//function authorFunction() {
+//
+//
+//    // Local Arrays
+//    var authors = [ "Charles Dickens", "William Shakespeare", "Mark Twain", "Hunter S. Thompson"];
+//    var titles = [ "Oliver Twist", "Hamlet", "Tom Sawyer", "Fear and Loathing in Las Vegas"];
+//
+//
+//    // For loop to go through array items
+//    // Output each element
+//    for (var i = 0; i < authors.length; i++) {
+//
+//        console.log(authors[i] + " wrote " + titles[i] + ".");
+//
+//    }
+//
+//    // Use an array method to alter the arrays
+//    authors.push("Dorothy Allison");
+//    titles.push("Bastard Out of Carolina");
+//
+//    // Do a final output of the change
+//    console.log("I added " + authors[i] + " who wrote " + titles[i] + ".");
+//    // You can also use authors[authors.length - 1]
+//
+//}
+//
+//// Main code
+//authorFunction();
