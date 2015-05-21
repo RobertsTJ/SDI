e15f1e6e2e554d1e6817ec74e734a90b7665c477
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-21 09:02:54 -0400

    Day 8 lecture coding setup
    set up for day 8 lecture coding

---
+++ Day8New_c9c79f32-50da-4e18-8504-69f4b36c7759/Day-8-lecture.js
@@ -0,0 +1,29 @@
+// SDI 1311
+//  Day 8 Lecture
+//  Like cats, we're going to play with strings
+
+// Using .indexOf()
+//  Find if the substring "Adam" appears in the string and where
+var myIndexOf = "Madam, I'm Adam.";
+
+// Using .lastIndexOf()
+//  Find if the substring "time" appears in the string and where
+var myLastIndexOf = "There is a time and a place for everything, and this is neither the time nor the place";
+
+// Using substr()
+//  Output the substring "Data" from the string
+var mysubStr = "This class is called Scalable Data Infrastructures";
+
+// Using substring()
+//  Output the substring between two index numbers in the string
+var mySubString = "Life, the Universe, and Everything!";
+
+// Using charAt()
+//  Find the character at index position 6
+var myEmail = "llewis@fullsail.com";
+
+// Using .split()
+//  Split a string into an array and output each item individually
+var mySplit = "Now is the time for all good men to come to the aid of the party.";
+
+
---
+++ sdi-LectureFiles-Day8/Day8Lecture.js
@@ -0,0 +1,7 @@
+/*  Timothy Roberts
+    SDI 1505
+    Day 8 Lecture
+    May 21, 2015
+ */
+
+// alert("JavaScript works!");
