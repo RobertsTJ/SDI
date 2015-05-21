d7116d169051e1c53acca6d2633c714bc30b4750
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-21 13:23:55 -0400

    Lab 8 Problem 1 Commit 1
    Code Set Up and working

---
+++ Roberts_Timothy_Lab8/Roberts_Timothy_Lab8.js
@@ -0,0 +1,30 @@
+/* Timothy Roberts
+   SDI 1505
+   Lab 8: Working with strings
+   May 21, 2015
+ */
+
+//alert("JavaScript works!");
+
+// Global Variable
+var myString;
+
+// Function
+function correctEmail(argString) {
+
+    // Local Variables
+    var newString;
+    var strArray;
+
+    newString = argString;
+
+
+    // Test Output
+    console.log(newString);
+
+}
+
+// Main Code
+ourString = prompt("Please enter your email address: ", "troberts@fullsail.edu");
+
+correctEmail(ourString);
