2876a1b08e16900a7f0c96f38d12754d11e8d08a
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-23 18:27:45 -0400

    Lab 9 Commit 3
    Coding for problem 3

--- Roberts_Timothy_Lab9/Roberts_Timothy_Lab9.js
+++ Roberts_Timothy_Lab9/Roberts_Timothy_Lab9.js
@@ -32,38 +32,83 @@
 // End
 
 
+//
+//// Problem 2
+//// Send string in to the function
+//// Return it as a number
+//// Determine if the string sent into the function is a number before you do the conversion.
+//
+//// Start
+//// Global Variables
+//var num = (prompt("Please enter a number: ", "987"));
+//var varReturn = stringNumber(num);
+//
+//// Function
+//function stringNumber(argPrompt) {
+//
+//    // Local Variables
+//    var returnedString;
+//
+//    if (isNaN(argPrompt)) {
+//
+//        returnedString = ("This is not a number.");
+//
+//    } else {
+//
+//        returnedString = parseFloat(argPrompt);
+//
+//    }
+//
+//    return returnedString;
+//
+//}
+//
+//// Main Code
+//console.log(varReturn);
+//
+//// End
 
-// Problem 2
-// Send string in to the function
-// Return it as a number
-// Determine if the string sent into the function is a number before you do the conversion.
+
+
+// Problem 3
+// Create a function that is the difference of two dates
+// Function requires 3 parameters: First two are dates to compare, 3rd is string to return hours or days
+// Function will do math to find the difference
+// Output in a meaningful way
 
 // Start
 // Global Variables
-var num = (prompt("Please enter a number: ", "987"));
-var varReturn = stringNumber(num);
+var oneDate = prompt("Please enter a date in this format: ", "05/23/2015");
+var twoDate = prompt("Please enter a date in this format: ", "05/31/2015");
+var dayOrHour = prompt("Please enter days or hours: ");
+
 
 // Function
-function stringNumber(argPrompt) {
+function dateDifferFunction (oneD, twoD) {
 
     // Local Variables
-    var returnedString;
+    var oneD = new Date(oneD);
+    var twoD = new Date(twoD);
+    var dateDiffer = oneD - twoD;
 
-    if (isNaN(argPrompt)) {
+    if (dayOrHour === "hours"){
 
-        returnedString = ("This is not a number.");
+        dateDiffer = Math.abs(dateDiffer / 1000 / 60/ 60);
+        console.log("You chose hours.")
 
-    } else {
+    } else if (dayOrHour === "days") {
 
-        returnedString = parseFloat(argPrompt);
+        dateDiffer = Math.abs(dateDiffer / 1000 / 60/ 60 / 24);
+        console.log("You chose days.")
 
     }
 
-    return returnedString;
+    return dateDiffer;
 
 }
 
 // Main Code
-console.log(varReturn);
+returnedDates = dateDifferFunction(oneDate,twoDate);
+console.log("There are " + returnedDates + " in between your chosen dates.");
 
 // End
