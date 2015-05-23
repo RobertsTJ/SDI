8090a260181b5b78125387948a3ce74f7ca8d1a4
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-23 10:32:40 -0400

    Day 9 Commit 6
    .random method example

--- sdi-LectureFiles-Day9/Day9Lecture.js
+++ sdi-LectureFiles-Day9/Day9Lecture.js
@@ -6,116 +6,163 @@
 
 // alert("JavaScript works!");
 
-// Review of lab 8 problem 1
-
-// Create a function
-// Check the string
-// Only 1 @ symbol
-// At least one dot after @
-// No spaces
-
-// Global Variables
-var myEmail= "troberts@fullsail.edu";
-var emailTrueOrFalse;
-
-// Problem Two
-var returnedString;
-var stringList = "a,b,c,d,e+f,g,h";
-var first = ",";
-var second = "/";
 
 
-// Problem Functions
-// First function: check email address
-function checkEmail(argEmail) {
+function mathMethods (max, min) {
+                                // Excludes max and includes min
+    var myRandom = Math.floor(Math.random() * (max - min) + min);
 
-    // Local Variables
-    var goodEmail = false;
-    var atSymbol = argEmail.indexOf("@");
-    var dotSymbol = argEmail.lastIndexOf(".");
-    var noSpaces = argEmail.indexOf(" ");
-    var lastAt = argEmail.lastIndexOf("@");
-
-    // Checking for @ symbol
-    if (atSymbol === lastAt && atSymbol != -1) {
-
-        goodEmail = true;
-
-    } else {
-
-        return false;
-    }
-
-    if (dotSymbol > atSymbol) {
-
-        goodEmail = true;
-
-    } else {
-
-        return false;
-
-    }
-
-    if (noSpaces == -1) {
-
-        goodEmail = true;
-
-    } else {
-
-        goodEmail = false;
-    }
-
-    return goodEmail;
+    console.log(myRandom);
 
 }
 
-// Review of lab 8 problem 2
-
-// Create a function
-// Accept 3 strings
-// List of items separated by character
-// The separator character
-// A new separator character
-// Take the first string, replace the initial separator with a new separator
-// Return the string
-
-// Split a string at a specified separator and replace it with a new separator
-function stringSep(oldString, firstSept, newSept) {
-
-    var newString = "";
-
-
-    for (var i = 0; i < oldString.length; i++) {
-
-        if (oldString.substr(i,1) === firstSept) {
-
-            newString = newString + newSept;
-
-        } else {
-
-            newString = newString + oldString.substr(i,1);
-
-
-        }
-
-        console.log(newString);
-
-    }
-
-    return newString;
-
-}
+mathMethods(50, 10);
+mathMethods(50,100);
 
 
 
 
 
 
-//// Main Code
-//emailTrueOrFalse = checkEmail(myEmail);
-//console.log("It is " + emailTrueOrFalse + " that this email is valid.");
 
 
-// Calls problem 2 function
-returnedString = stringSep(stringList, first, second);
-console.log("The new string is " + returnedString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//// Review of lab 8 problem 1
+//
+//// Create a function
+//// Check the string
+//// Only 1 @ symbol
+//// At least one dot after @
+//// No spaces
+//
+//// Global Variables
+//var myEmail= "troberts@fullsail.edu";
+//var emailTrueOrFalse;
+//
+//// Problem Two
+//var returnedString;
+//var stringList = "a,b,c,d,e+f,g,h";
+//var first = ",";
+//var second = "/";
+//
+//
+//// Problem Functions
+//// First function: check email address
+//function checkEmail(argEmail) {
+//
+//    // Local Variables
+//    var goodEmail = false;
+//    var atSymbol = argEmail.indexOf("@");
+//    var dotSymbol = argEmail.lastIndexOf(".");
+//    var noSpaces = argEmail.indexOf(" ");
+//    var lastAt = argEmail.lastIndexOf("@");
+//
+//    // Checking for @ symbol
+//    if (atSymbol === lastAt && atSymbol != -1) {
+//
+//        goodEmail = true;
+//
+//    } else {
+//
+//        return false;
+//    }
+//
+//    if (dotSymbol > atSymbol) {
+//
+//        goodEmail = true;
+//
+//    } else {
+//
+//        return false;
+//
+//    }
+//
+//    if (noSpaces == -1) {
+//
+//        goodEmail = true;
+//
+//    } else {
+//
+//        goodEmail = false;
+//    }
+//
+//    return goodEmail;
+//
+//}
+//
+//// Review of lab 8 problem 2
+//
+//// Create a function
+//// Accept 3 strings
+//// List of items separated by character
+//// The separator character
+//// A new separator character
+//// Take the first string, replace the initial separator with a new separator
+//// Return the string
+//
+//// Split a string at a specified separator and replace it with a new separator
+//function stringSep(oldString, firstSept, newSept) {
+//
+//    var newString = "";
+//
+//
+//    for (var i = 0; i < oldString.length; i++) {
+//
+//        if (oldString.substr(i,1) === firstSept) {
+//
+//            newString = newString + newSept;
+//
+//        } else {
+//
+//            newString = newString + oldString.substr(i,1);
+//
+//
+//        }
+//
+//        console.log(newString);
+//
+//    }
+//
+//    return newString;
+//
+//}
+//
+//
+//
+//
+//
+//
+////// Main Code
+////emailTrueOrFalse = checkEmail(myEmail);
+////console.log("It is " + emailTrueOrFalse + " that this email is valid.");
+//
+//
+//// Calls problem 2 function
+//returnedString = stringSep(stringList, first, second);
+//console.log("The new string is " + returnedString);
