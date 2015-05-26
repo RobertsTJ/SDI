5120c52cbbc7df3a4a534f797799ae9121be0596
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-26 13:14:10 -0400

    Lab 10 Commit 1
    Initial code setup for lab 10

---
+++ Roberts_Timothy_Lab10/Roberts_Timothy_Lab10.js
@@ -0,0 +1,29 @@
+/* Timothy Roberts
+   SDI 1505
+   May 26, 2015
+   Lab 10: Problem-solving
+ */
+
+// alert("JavaScript works!");
+
+
+// Start
+// Global Variables
+var returnedQuickPick;
+
+
+
+// Function
+function lotteryFunction () {
+
+    // Local Variables
+    var quickPick = prompt("Would you like to play the Powerball or the Florida Lottery?")
+
+
+}
+
+
+
+// Main Code
+returnedQuickPick = lotteryFunction();
+console.log("Your quick pick numbers are " + returnedQuickPick);
