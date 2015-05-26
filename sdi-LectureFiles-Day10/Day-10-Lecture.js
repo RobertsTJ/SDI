67b50184d363293263ebbb6ecd948322cc46c3b9
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-26 09:03:53 -0400

    Day 10 Commit 1
    Set up coding files for day 10 lecture

---
+++ sdi-LectureFiles-Day10/Day-10-Lecture.js
@@ -0,0 +1,19 @@
+/*   Timothy Roberts
+     SDI 1505
+     May 26, 2015
+     Day 10 Lecture Coding
+ */
+
+// alert("JavaScript works!");
+
+/*
+    Create a function that accepts a username and password and then checks
+    that against a list of data in the JSON to determine if the user exists
+    and is allowed to login with the password provided.  The function should
+    only return a string with the first name of the user if the username and
+    password are correct.  It should return a string indicating failure if
+    the input is wrong.  The code will then output that string.
+*/
+
+
+
