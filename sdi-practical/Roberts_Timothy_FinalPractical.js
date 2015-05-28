21f0693226eedfeb9d798669e91a6f943c1c7596
Tim Roberts <Timothy@Timothys-MacBook-Pro.local>
2015-05-28 09:07:48 -0400

    Day 11 Final Practical Commit 1
    Comments set up for code

---
+++ sdi-practical/Roberts_Timothy_FinalPractical.js
@@ -0,0 +1,22 @@
+/* Timothy Roberts
+   SDI: 1505
+   May 28, 2015
+   Final Practical Exam
+ */
+
+// alert("JavaScript works!");
+
+// Start
+// Global Variables
+
+
+
+// Function
+
+
+
+
+// Main Code
+
+
+// End
